714983669f5cb23b1f0586616d2a16097c771345
farouqdaif1 <daiffarouq@gmail.com>
2025-03-29 21:48:44 +0200

    Add shad cn

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import Dashboard from "./app/dashboard/dashboard";
+
+function App() {
+  return (
+    <div className="dark">
+      <Dashboard />
+    </div>
+  );
+}
+
+export default App;
